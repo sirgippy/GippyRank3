9067db02436bdb1861adedb47fae02420c8a45be
sirgippy <jegipson@gmail.com>
2014-11-02 15:43:16 -0600

    rearranged

--- src/Team.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'SirGippy'
-
-class Team:
-    def __init__(self,name):
-        self.name = name
-        self.season = []
-
-    def addGame(self,game):
-        self.season.append(game)
